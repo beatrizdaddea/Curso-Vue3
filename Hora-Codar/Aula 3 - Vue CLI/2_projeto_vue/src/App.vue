054d70d0b7fa7af3c318e54b8faa6683c399ccc7
beatrizdaddea <beatrizchith@gmail.com>
2023-06-23 14:46:49 -0300

    LifeCycle

--- Hora-Codar/Aula 3 - Vue CLI/2_projeto_vue/src/App.vue
+++ Hora-Codar/Aula 3 - Vue CLI/2_projeto_vue/src/App.vue
@@ -1,14 +1,19 @@
 <template>
+  <div>
     <PrimeiroComponente />
-  </template>
+    <LifeCycle />
+  </div>
+</template>
 
-  <script>
-  import PrimeiroComponente from './components/PrimeiroComponente.vue'
+<script>
+import PrimeiroComponente from './components/PrimeiroComponente.vue'
+import LifeCycle from './components/LifeCycle.vue'
 
-  export default {
-    name: 'App',
-    components: {
-      PrimeiroComponente
-    }
+export default {
+  name: 'App',
+  components: {
+    PrimeiroComponente,
+    LifeCycle
   }
-  </script>
+}
+</script>
---
+++ Hora-Codar/Aula 3 - Vue CLI/2_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,33 @@
+<template>
+    <div> 
+        <h1>Meu nome é {{ nome }}</h1>
+        <p> Sou desenvovedor {{ trabalho }} </p>
+    </div>
+
+  </template>
+
+  <script>
+  export default {
+    name: 'LifeCycle',
+    data() {
+      return {
+        nome: "Ainda não sei",
+        trabalho: "Não sei"
+      }
+    },
+    created() {
+        setTimeout(() => {
+            this.nome = "Matheus"
+            this.trabalho = "Mobile"
+        }, 1000);
+    },
+
+    mounted(){
+        setTimeout(() => {
+            this.nome = "Pedro"
+            this.trabalho = "Front End"
+        }, 2000);
+    }
+
+    }
+  </script>

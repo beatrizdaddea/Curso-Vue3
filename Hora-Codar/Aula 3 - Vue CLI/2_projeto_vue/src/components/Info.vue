38a47f3cfdab224eeb7609b99a8ac18cbcc324b1
beatrizdaddea <beatrizchith@gmail.com>
2023-06-27 11:54:33 -0300

    aula 8 - hierarquia

---
+++ Hora-Codar/Aula 3 - Vue CLI/2_projeto_vue/src/components/Info.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p> Estou trabalahndo no momento.</p>
+        <p> Utilizado as seguintes tecnologias: </p>
+        <ul>
+            <li> JavaScript</li>
+            <li> PHP </li>
+            <li> Python </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+  export default{
+        name: "Info",
+    }
+</script>
---
+++ Hora-Codar/Aula 3 - Vue CLI/2_projeto_vue/src/components/Pessoa.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2> Está é a descrição da pessoa: {{ nome }}</h2>
+        <Info/>
+    </div>
+</template>
+
+<script>
+import Info from './Info.vue'
+
+    export default{
+        name: "pessoa",
+        components:{
+            Info,
+        },
+        data(){
+            return{
+                nome: "Beatriz"
+            }
+        }
+    }
+</script>

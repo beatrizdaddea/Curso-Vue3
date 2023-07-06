280b3835988c571b623026ccd56680cf00cbb01c
beatrizdaddea <beatrizchith@gmail.com>
2023-07-06 14:39:57 -0300

    Eventos

--- Hora-Codar/Aula 3 - Vue CLI/2_projeto_vue/src/components/Form.vue
+++ Hora-Codar/Aula 3 - Vue CLI/2_projeto_vue/src/components/Form.vue
@@ -1,19 +1,21 @@
 <template>
     <div>
         <h2> Meu Formulário </h2>
-    <form action="">
+    <form action="" @submit="enviarFormulario($event)">
         <div>
-            <InputText/>
+            <input type="text" v-model="name">
         </div>
         <div>
-            <InputText/>
+            <input type="text" v-model="email">
         </div>
         <div>
             <Submit/>
         </div>
     </form>
-    </div>
+    <p> Conteúdo</p>
+    <p> Conteúdo</p>
 
+    </div>
 </template>
 
 <script>
@@ -25,7 +27,31 @@ import Submit from './form/Submit.vue';
         components: {
             InputText,
             Submit
+        },
+        data(){
+            return{
+                name: "",
+                email: ""
+            }
+        },
+        methods:{
+            enviarFormulario(e) {
+                e.preventDefault();
+                const name = this.name;
+                const email = this.email;
+                console.log('Formulario enviado')
+                console.log("O nome é: " + name)
+                console.log("O email é: " + email)
+
+            }
         }
     }
 
 </script>
+
+<style scoped>
+    p{
+        margin: 150px;
+    }
+
+</style>
